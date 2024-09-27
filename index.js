0d77149d01746250be06ef699c00f0e51a9b7646
anjanshrestha8 <anjanshrestha2023@gmail.com>
2024-09-27 21:23:03 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 8000;
+app.use(express.json());
+app.use(cors());
+
+app.post("/", (request, response) => {
+  const code = request.body.code;
+  if (code.length !== 6 || code[5] === "7") {
+    return response.status(400).json({ message: "Validation error" });
+  } else {
+    return response.status(200).json({ message: "Successful" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running at :", PORT);
+});
